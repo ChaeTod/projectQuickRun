7420c8eabcf76c3798d31e73688d8518980bf7cc
Artem Kozyr <zymber6@gmail.com>
2020-07-28 15:34:57 +0200

    And another words

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.printf("Say hello to my little friend!");
+        System.out.printf("Have you something to sat?");
     }
 }
